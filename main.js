dea611bee44c9911aa9823d50861170bec04064b
hnjilee <hnjilee@gmail.com>
2023-04-19 17:05:13 +0900

    Apply BEM again to prevent naming conflicts

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const toggleBtn = document.querySelector('.toggle');
-const menu = document.querySelector('.menu');
-const links = document.querySelector('.links');
+const toggleBtn = document.querySelector('.header__toggle');
+const menu = document.querySelector('.header__menu');
+const links = document.querySelector('.header__links');
 
 toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('menu--exposed');
-  links.classList.toggle('links--exposed');
+  menu.classList.toggle('header__menu--exposed');
+  links.classList.toggle('header__links--exposed');
 });
